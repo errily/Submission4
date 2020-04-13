25dfdbb88594c61d7d2792a0dea6224dd6a9784f
errily <erril.98@gmail.com>
2020-04-13 22:29:00 +0700

    Create kelas.js

---
+++ kelas.js
@@ -0,0 +1,54 @@
+var readline = require('readline-sync');
+class Segitiga {
+constructor(a, t) {
+// mendefinisikan alas dan tinggi
+this.alas = a;
+this.tinggi = t;
+}
+luas() {
+return this.alas * this.tinggi / 2;
+}
+}
+// membaca data alas dan tinggi
+var a = parseFloat(readline.question(' Masukan alas: '));
+var t = parseFloat(readline.question(' Masukan tinggi: '));
+
+// membuat objek dari kelas segitiga
+
+var obj = new Segitiga(a, t);
+
+console.log(' \nSetelah nilai properti diubah ');
+console.log(` obj.alas\t: ${obj.alas} `);
+console.log(` obj.tinggi\t: ${obj.tinggi} `);
+console.log(' obj.luas\t: ' +  obj.luas() );
+
+console.log('metode statis')
+console.log('\n')
+
+var readline = require('readline-sync');
+class Segitiga1 {
+
+constructor(a, t) {
+// mendefinisikan properti
+this.alas = a;
+this.tinggi = t;
+}
+luas() {
+return this.alas * this.tinggi / 2;
+}
+// metode statis
+static buatObjek(a, t) {
+return new Segitiga1(a, t);
+}
+}
+// membaca data alas dan tinggi
+var a = parseFloat(readline.question(' Masukan alas: '));
+var t = parseFloat(readline.question(' Masukan tinggi: '));
+
+// memanggil metode statis
+var obj = Segitiga1.buatObjek(a, t);
+
+console.log(' \nSetelah nilai properti diubah ');
+console.log(` obj.alas\t: ${obj.alas} `);
+console.log(` obj.tinggi\t: ${obj.tinggi} `);
+console.log(' obj.luas\t: ' +  obj.luas() );
